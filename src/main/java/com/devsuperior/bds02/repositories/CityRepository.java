e125b3f294fec563f86523db54f1b901f1d7494a
pdcase_acunha <pdcase_acunha@banpara.com>
2022-12-03 17:05:16 -0300

    feature: criando servico, repository, controller city e regra de negocio para o findAllShouldReturnAllResourcesSortedByName

---
+++ src/main/java/com/devsuperior/bds02/repositories/CityRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.bds02.repositories;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+}
