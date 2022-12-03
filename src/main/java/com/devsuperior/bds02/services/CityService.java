dec9b6e8e368584a1cced79a7b56e5c201347499
pdcase_acunha <pdcase_acunha@banpara.com>
2022-12-03 17:40:53 -0300

    feature: resolvendo testes delete

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -3,7 +3,11 @@ package com.devsuperior.bds02.services;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -26,10 +30,22 @@ public class CityService {
         return city.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional
     public CityDTO insert(CityDTO dto){
         City entity = new City();
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CityDTO(entity);
+    }
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
     }
 }
